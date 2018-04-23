ec3c84278afcdfb56555901023530c9659ba47ac
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraPrinting;
+using DevExpress.XtraReports.UI;
+using System.Collections;
+
+namespace WindowsApplication1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            XtraReport1 r = new XtraReport1();
+            r.CreateDocument();
+            ShowControlsOnPage(r, 2);
+            //r.ShowPreviewDialog();
+        }
+
+
+        private void ShowControlsOnPage(XtraReport1 report, int pageNumber) {
+            if(report.PrintingSystem.Document.PageCount >= pageNumber - 1) {
+                IDictionary controls = GetControlsOnPage(report, pageNumber);
+                ArrayList al = CopyToArrayList(controls);
+
+                XtraReport r= new XtraReport();
+                r.DataSource = al;
+                DetailBand db = new DetailBand();
+                db.Height = 20;
+                r.Bands.Add(db);
+                XRLabel l = new XRLabel();
+                l.DataBindings.Add("Text", null, "FullName");
+                l.Location = new Point(0, 0);
+                l.Size = new Size(500, 20);
+                db.Controls.Add(l);
+                ReportPrintTool tool = new ReportPrintTool(r);
+                tool.ShowPreviewDialog();
+            }
+        }
+
+        private ArrayList CopyToArrayList(IDictionary controls) {
+            ArrayList al = new ArrayList();
+            foreach(DictionaryEntry obj in controls) {
+                al.Add(obj.Value);
+            }
+            return al;
+        }
+
+        private static IDictionary GetControlsOnPage(XtraReport1 r, int pageNumber) {
+            IDictionary controls = new Hashtable();
+            Page p = r.PrintingSystem.Document.Pages[pageNumber - 1];
+            BrickEnumerator be = p.GetEnumerator();
+            while(be.MoveNext()) {
+                VisualBrick vb = be.Current as VisualBrick;
+                MObject mo = new MObject(((XRControl)vb.BrickOwner).Name, ((XRControl)vb.BrickOwner).Report.ToString());
+                if(!controls.Contains(mo.FullName))
+                    controls.Add(mo.FullName, mo);
+            }
+            return controls;
+        }
+    }
+
+    class MObject {
+        public MObject(string n, string r) { objname = n; reportName = r; }
+
+        string objname;
+        public string ObjName {
+            get { return objname; }
+            set { objname = value; }
+        }
+        string reportName;
+
+        public string ReportName {
+            get { return reportName; }
+            set { reportName = value; }
+        }
+        public string FullName {
+            get { return reportName + "." + objname; }
+        }
+
+    }
+}
---
+++ VB/XtraReport1.Designer.vb
@@ -0,0 +1,182 @@
+Imports Microsoft.VisualBasic
+Imports System
+Namespace WindowsApplication1
+	Partial Public Class XtraReport1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+			Me.xrSubreport1 = New DevExpress.XtraReports.UI.XRSubreport()
+			Me.xtraReport21 = New WindowsApplication1.XtraReport2()
+			Me.xrLabel1 = New DevExpress.XtraReports.UI.XRLabel()
+			Me.xrTable1 = New DevExpress.XtraReports.UI.XRTable()
+			Me.xrTableRow1 = New DevExpress.XtraReports.UI.XRTableRow()
+			Me.xrTableCell1 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.xrTableCell2 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.xrTableCell3 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.PageHeader = New DevExpress.XtraReports.UI.PageHeaderBand()
+			Me.PageFooter = New DevExpress.XtraReports.UI.PageFooterBand()
+			Me.nwindDataSet1 = New WindowsApplication1.nwindDataSet()
+			Me.categoriesTableAdapter = New WindowsApplication1.nwindDataSetTableAdapters.CategoriesTableAdapter()
+			Me.topMarginBand1 = New DevExpress.XtraReports.UI.TopMarginBand()
+			Me.bottomMarginBand1 = New DevExpress.XtraReports.UI.BottomMarginBand()
+			CType(Me.xtraReport21, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.xrTable1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.nwindDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' Detail
+			' 
+			Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrSubreport1, Me.xrLabel1, Me.xrTable1})
+			Me.Detail.HeightF = 75F
+			Me.Detail.Name = "Detail"
+			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' xrSubreport1
+			' 
+			Me.xrSubreport1.LocationFloat = New DevExpress.Utils.PointFloat(42F, 50F)
+			Me.xrSubreport1.Name = "xrSubreport1"
+			Me.xrSubreport1.ReportSource = Me.xtraReport21
+			Me.xrSubreport1.SizeF = New System.Drawing.SizeF(100F, 25F)
+			' 
+			' xrLabel1
+			' 
+			Me.xrLabel1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Categories.CategoryID")})
+			Me.xrLabel1.LocationFloat = New DevExpress.Utils.PointFloat(550F, 25F)
+			Me.xrLabel1.Name = "xrLabel1"
+			Me.xrLabel1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrLabel1.SizeF = New System.Drawing.SizeF(150F, 25F)
+			Me.xrLabel1.Text = "xrLabel1"
+			Me.xrLabel1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' xrTable1
+			' 
+			Me.xrTable1.LocationFloat = New DevExpress.Utils.PointFloat(0F, 0F)
+			Me.xrTable1.Name = "xrTable1"
+			Me.xrTable1.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 96F)
+			Me.xrTable1.Rows.AddRange(New DevExpress.XtraReports.UI.XRTableRow() { Me.xrTableRow1})
+			Me.xrTable1.SizeF = New System.Drawing.SizeF(642F, 23F)
+			Me.xrTable1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' xrTableRow1
+			' 
+			Me.xrTableRow1.Cells.AddRange(New DevExpress.XtraReports.UI.XRTableCell() { Me.xrTableCell1, Me.xrTableCell2, Me.xrTableCell3})
+			Me.xrTableRow1.Name = "xrTableRow1"
+			Me.xrTableRow1.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 96F)
+			Me.xrTableRow1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			Me.xrTableRow1.Weight = 1
+			' 
+			' xrTableCell1
+			' 
+			Me.xrTableCell1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Categories.CategoryID")})
+			Me.xrTableCell1.Name = "xrTableCell1"
+			Me.xrTableCell1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrTableCell1.Text = "xrTableCell1"
+			Me.xrTableCell1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			Me.xrTableCell1.Weight = 0.33021806853582553
+			' 
+			' xrTableCell2
+			' 
+			Me.xrTableCell2.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Categories.CategoryName")})
+			Me.xrTableCell2.Name = "xrTableCell2"
+			Me.xrTableCell2.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrTableCell2.Text = "xrTableCell2"
+			Me.xrTableCell2.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			Me.xrTableCell2.Weight = 0.33021806853582553
+			' 
+			' xrTableCell3
+			' 
+			Me.xrTableCell3.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Categories.Description")})
+			Me.xrTableCell3.Name = "xrTableCell3"
+			Me.xrTableCell3.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrTableCell3.Text = "xrTableCell3"
+			Me.xrTableCell3.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			Me.xrTableCell3.Weight = 0.33956386292834889
+			' 
+			' PageHeader
+			' 
+			Me.PageHeader.HeightF = 30F
+			Me.PageHeader.Name = "PageHeader"
+			Me.PageHeader.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.PageHeader.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' PageFooter
+			' 
+			Me.PageFooter.HeightF = 30F
+			Me.PageFooter.Name = "PageFooter"
+			Me.PageFooter.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.PageFooter.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' nwindDataSet1
+			' 
+			Me.nwindDataSet1.DataSetName = "nwindDataSet"
+			Me.nwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+			' 
+			' categoriesTableAdapter
+			' 
+			Me.categoriesTableAdapter.ClearBeforeFill = True
+			' 
+			' topMarginBand1
+			' 
+			Me.topMarginBand1.HeightF = 100F
+			Me.topMarginBand1.Name = "topMarginBand1"
+			' 
+			' bottomMarginBand1
+			' 
+			Me.bottomMarginBand1.HeightF = 100F
+			Me.bottomMarginBand1.Name = "bottomMarginBand1"
+			' 
+			' XtraReport1
+			' 
+			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.PageHeader, Me.PageFooter, Me.topMarginBand1, Me.bottomMarginBand1})
+			Me.DataAdapter = Me.categoriesTableAdapter
+			Me.DataMember = "Categories"
+			Me.DataSource = Me.nwindDataSet1
+			Me.Version = "9.3"
+			CType(Me.xtraReport21, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.xrTable1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.nwindDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private Detail As DevExpress.XtraReports.UI.DetailBand
+		Private PageHeader As DevExpress.XtraReports.UI.PageHeaderBand
+		Private PageFooter As DevExpress.XtraReports.UI.PageFooterBand
+		Private nwindDataSet1 As nwindDataSet
+		Private categoriesTableAdapter As WindowsApplication1.nwindDataSetTableAdapters.CategoriesTableAdapter
+		Private xrLabel1 As DevExpress.XtraReports.UI.XRLabel
+		Private xrTable1 As DevExpress.XtraReports.UI.XRTable
+		Private xrTableRow1 As DevExpress.XtraReports.UI.XRTableRow
+		Private xrTableCell1 As DevExpress.XtraReports.UI.XRTableCell
+		Private xrTableCell2 As DevExpress.XtraReports.UI.XRTableCell
+		Private xrTableCell3 As DevExpress.XtraReports.UI.XRTableCell
+		Private xrSubreport1 As DevExpress.XtraReports.UI.XRSubreport
+		Private xtraReport21 As XtraReport2
+		Private topMarginBand1 As DevExpress.XtraReports.UI.TopMarginBand
+		Private bottomMarginBand1 As DevExpress.XtraReports.UI.BottomMarginBand
+	End Class
+End Namespace
